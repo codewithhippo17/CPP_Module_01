13936c70ffd14393e5c9d4c3793486b35a249ec2
ehamza <ehamza@student.1337.ma>
2026-01-10 11:43:46 +0100

    refactor(ex01): enhance the main test by printing which zombie is announcing

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,13 +1,13 @@
 #include "Zombie.hpp"
 
-int
-main ( ) {
+int main() {
 
-	int N = 10;
-	Zombie* horde = zombieHorde( N, "hippo" );
-	for ( int i = 0; i < N; i++ ) {
-		horde[i].announce();
-	}
-	delete[] horde;
-	return 0;
+  int N = 10;
+  Zombie *horde = zombieHorde(N, "hippo");
+  for (int i = 0; i < N; i++) {
+    std::cout << "Zombie[" << i + 1 << "] :";
+    horde[i].announce();
+  }
+  delete[] horde;
+  return 0;
 }
